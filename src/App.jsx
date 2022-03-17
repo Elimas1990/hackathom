22d7f1fb4feb0da3294f6cff5e03253b635b7553
Elimas1990 <estebanlimas@gmail.com>
2022-03-17 10:56:41 -0300

    Se suma sass, bootstrap y fontawesome

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from 'react-router-dom';
+import './App.scss';
+
+function App() {
+  return (
+    
+    <BrowserRouter>
+
+    </BrowserRouter>
+      
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js'
 
 ReactDOM.render(
   <React.StrictMode>
